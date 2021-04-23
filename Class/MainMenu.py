f7abeafc9b8b1319e0184a71fdea53e388119703
Guin <Justis.Guin@evsck12.com>
2021-04-23 07:41:14 -0500

    Update

--- Class/MainMenu.py
+++ Class/MainMenu.py
@@ -11,13 +11,11 @@ t.bgcolor("Light Gray")
 
 pen = t.Turtle()
 pen.speed(0)
-#-----------------------------------------------------------
-wn.screensize(360,420)
-boardMaker=t.Turtle()
-#boardMaker.hideturtle()
-boardMaker.penup()
-boardMaker.speed(0)
+#----------------------------------------------------------
+wn.screensize(600,600)
 
+
+'''
 tess=t.Turtle()  
   # self defined function to print coordinate
 def buttonclick(x,y): 
@@ -29,15 +27,34 @@ t.listen()  # listen to incoming connections
 t.speed(10) # set the speed
     # hold the screen
 
-
+'''
 pen.penup()
-pen.goto(-50.0,318.0)
+pen.goto(-50.0,308.0)
 pen.write("Main Menu", font=("Arial",30,"normal"))
 
 pen.penup()
 pen.goto(-361.0,130.0)
 pen.write("Player Vs Computer", font=("Arial",25,"normal"))
 
+def Computer():
+     
+
+def Player():
+      
+
+pen.goto(-361.0,100.0)
+pen.write("Press C to play Player Vs Computer",font=("Arial",15,"normal"))
+
+pen.penup()
+pen.goto(250.0,130.0)
+pen.write("Player Vs Player", font=("Arial",25,"normal"))
+
+pen.goto(250.0,100.0)
+pen.write("Press P to see Player vs Player Directions",font=("Arial",15,"normal"))
+
+
+
+#player vs Computer Box 
 pen.goto(-62.0,168.0)
 pen.down()
 pen.left(180)
@@ -50,21 +67,21 @@ pen.right(-90)
 pen.forward(40)
 pen.penup()
 
+#Player Vs Player Box 
 pen.goto(242.0,166.0)
 pen.down()
 pen.right(90)
 pen.forward(250)
 pen.right(90)
 pen.forward(40)
-pen.
+pen.left(-90)
+pen.forward(250)
+pen.right(90)
+pen.forward(40)
 
 
-
-pen.penup()
-pen.goto(250.0,130.0)
-pen.write("Player Vs Player", font=("Arial",25,"normal"))
-
-
+wn.onkey(Computer, 'C')
+wn.onkey(Player, 'Left')
 
 
 wn.mainloop()
